701f64cd335963b014ddb7a09d371c49d040a9b4
Behnam Sabaghi <behnamsabaghi@gmail.com>
2018-01-30 01:09:17 +0330

    commented

--- main.cpp
+++ main.cpp
@@ -2,13 +2,19 @@
 #include <iostream>
 int main()
 {
+	// set print level to top ( print all logs )
 	FLog::printAll = true;
+
+	// set log level to top ( log every thing )
 	FLog::logLevel = FLog::Level::all;
+
+	// using APIs
 	FLogUser("first user", "Test message log");
 	FLogGui("Test message log");
 	FLogerror("Test message log");
 	FLogdatabase("Test message log");
 	FLoginfo("Test message log");
 	FLogwarning("Test message log");
+
 	return 0;
 }
